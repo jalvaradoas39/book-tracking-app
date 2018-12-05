875d0d98261fe31b5ed6d58b82d9373047ada994
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-12-05 13:25:08 -0500

    Add container for search results of API

--- src/App.js
+++ src/App.js
@@ -19,12 +19,17 @@ class BooksApp extends React.Component {
 
 
           <div className="search-books">
+
             <div className="search-books-bar">
               <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
 
               <div className="search-books-input-wrapper">
                 <input type="text" placeholder="Search by title or author" />
               </div>
+            </div>
+
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
             </div>
           </div>
 
