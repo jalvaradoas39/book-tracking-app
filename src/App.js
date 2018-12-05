5d2261781b4515c514f125cbe544971c8f9bb332
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-12-05 11:13:27 -0500

    Add render() to BooksApp component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,28 @@ class BooksApp extends React.Component {
 
 
 
+  render() {
+    return (
+      <div className="app">
+
+        {this.state.showSearchPage ? (
+
+          <div className="search-books">
+            <h1>Inside search books</h1>
+          </div>
+
+        ) : (
+
+          <div className="list-books">
+            <h1>Inside list books</h1>
+          </div>
+
+        )}
+
+      </div>
+    )
+  }
+
 }
 
 
