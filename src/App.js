a4350dd693c2dbb1e6c3a1df8cd4de449b93d316
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-12-05 20:29:50 -0500

    Fetch all books inside componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React from 'react'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 
 class App extends React.Component {
 
+  state = {
+    myReads: [],
+    searchedBooks: []
+  }
 
 
+  componentDidMount() {
+    BooksAPI.getAll().then(myReads => {
+      this.setState({myReads})
+    })
+  }
 
 
-
-
-
+  render() {
+    return (
+      <div className="app">
+        
+      </div>
+    )
+  }
 
 
 }
