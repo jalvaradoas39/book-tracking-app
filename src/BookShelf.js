88ae0e1d26b3aaa4e019ba11ea4402c5b1f80dd4
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-12-10 13:50:36 -0500

    Create BookShelf.js component to insert inside HomePage.js

---
+++ src/BookShelf.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+
+class BookShelf extends React.Component {
+
+
+
+	render() {
+		return (
+
+			<div className="bookshelf">
+
+
+				
+			</div>
+
+		)
+	}
+
+}
+
+export default BookShelf;
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BookShelf from './BookShelf';
 
 
 
@@ -17,7 +18,7 @@ class HomePage extends React.Component {
 				</div>
 
 				<div className="list-books-content">
-					
+
 				</div>
 
 				<div className="open-search">
