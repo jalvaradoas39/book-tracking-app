aff7694a2f98bb0cb865c03136d9dc31d648cb79
Jorge Alvarado <jalvaradoas39@outlook.com>
2018-12-10 12:14:15 -0500

    Add 3 sections to HomePage.js

--- src/HomePage.js
+++ src/HomePage.js
@@ -7,7 +7,20 @@ class HomePage extends React.Component {
 
 	render() {
 		return (
-			<h1>Rendering Home Page</h1>
+			// <h1>Rendering Home Page</h1>
+			<div className="list-books">
+				<div className="list-books-title">
+					
+				</div>
+
+				<div className="list-books-content">
+					
+				</div>
+
+				<div className="open-search">
+					
+				</div>
+			</div>
 		)
 	}
 
